dcce172ef05fc87287c6d03cb3e1de28182753d7
dileepthoutam <thoutamdileep@gmail.com>
2025-06-24 20:35:24 +0530

    Added an input form via UI.

--- src/main/java/com/dileep/urlshortener/UrlController.java
+++ src/main/java/com/dileep/urlshortener/UrlController.java
@@ -4,18 +4,24 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Map;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping
 public class UrlController {
 
     @Autowired
     private UrlShorteningService shorteningService;
+
+    @GetMapping
+    public String urlShortener() {
+        return "urlshortener";
+    }
 
     @PostMapping("/shorten")
     public ResponseEntity<String> shorten(@RequestBody Map<String, String> body, HttpServletRequest request) {
--- src/main/java/com/dileep/urlshortener/UrlMappingRepo.java
+++ src/main/java/com/dileep/urlshortener/UrlMappingRepo.java
@@ -11,6 +11,6 @@ public interface UrlMappingRepo extends JpaRepository<UrlMappingEntity, Long> {
     @Query("SELECT u FROM UrlMappingEntity u WHERE u.shortCode = :shortCode")
     public Optional<UrlMappingEntity> findByShortCode(String shortCode);
 
-    @Query("SELECT u FROM UrlMappingEntity u WHERE u.longUrl = longUrl")
+    @Query("SELECT u FROM UrlMappingEntity u WHERE u.longUrl = :longUrl")
     public Optional<UrlMappingEntity> doesLongUrlExist(String longUrl);
 }
